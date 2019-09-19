700c7351bbf9cf1decb59df177befc06a3076bc6
CarnunMP <carnun@hotmail.co.uk>
2019-09-19 15:17:42 +0100

    validation mostly done: will come back to it!

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,40 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import axios from "axios";
 
 export default function FormikForm(props) {
+    const initialValues = {
+        name: "",
+        email: "",
+        password: "",
+    }
+
     return (
-        <Formik 
+        <Formik
+            initialValues={initialValues}
+            onSubmit={onSubmit}
+            validationSchema={validationSchema}
             render={props => {
                 return(
                     <Form>
                         <div>
                             <Field name="name" type="text" placeholder="Name" />
+                            <ErrorMessage name='name' component='div' />
                         </div>
                         <div>
                             <Field name="email" type="text" placeholder="Email" />
+                            <ErrorMessage name='email' component='div' />
                         </div>
                         <div>
-                            <Field name="password" type="text" placeholder="Password" />
+                            <Field name="password" type="password" placeholder="Password" />
+                            <ErrorMessage name='password' component='div' />
                         </div>
                         <div>
-                            <Field name="tos" type="checkbox" placeholder="Name" />
+                            I agree to the Terms of Service
+                            <Field name="ToS" type="checkbox"/>
+                            <ErrorMessage name='ToS' component='div' />
+                            {/* To-Do: This error isn't showing... */}
                         </div>
                         <button type="submit">Submit</button>
                     </Form>
@@ -26,3 +43,26 @@ export default function FormikForm(props) {
         />
     )
 }
+
+const onSubmit = (formValues, actions) => {
+    const userToPost = {
+        name: formValues.name,
+        email: formValues.email,
+        password: formValues.password,
+    };
+
+    axios.post("https://reqres.in/api/users")
+        .then(response => {
+            debugger
+        })
+        .catch(error => {
+            debugger
+        })
+};
+
+const validationSchema = yup.object().shape({
+    name: yup.string().required("Please enter a name."),
+    email: yup.string().required("Please enter an email adresss."),
+    password: yup.string().required("Please enter a password."),
+    ToS: yup.bool().required("You cannot submit unless you have agrees to the Terms of Service."),
+});
