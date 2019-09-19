8ecf4a87833b28e027657cb9d010c407b6f71492
CarnunMP <carnun@hotmail.co.uk>
2019-09-19 13:37:42 +0100

    basic formik form created

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,28 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-export default function Form(props) {
+export default function FormikForm(props) {
     return (
-        <>
-        </>
+        <Formik 
+            render={props => {
+                return(
+                    <Form>
+                        <div>
+                            <Field name="name" type="text" placeholder="Name" />
+                        </div>
+                        <div>
+                            <Field name="email" type="text" placeholder="Email" />
+                        </div>
+                        <div>
+                            <Field name="password" type="text" placeholder="Password" />
+                        </div>
+                        <div>
+                            <Field name="tos" type="checkbox" placeholder="Name" />
+                        </div>
+                        <button type="submit">Submit</button>
+                    </Form>
+                )
+            }}
+        />
     )
 }
